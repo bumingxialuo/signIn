7edafb7592e45cb51996de64151aa25671650a2b
bumingxialuo <690148814@qq.com>
2017-06-08 21:18:38 +0800

    Initial Commit

---
+++ signIn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  signIn
+//
+//  Created by apple7 on 2017/6/8.
+//  Copyright © 2017年 apple7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ signIn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  signIn
+//
+//  Created by apple7 on 2017/6/8.
+//  Copyright © 2017年 apple7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
